2d279eca7983f452659fe4c1f53b03af4aa05c0f
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-06-21 18:06:37 +0200

    Update ReminderException.java

--- src/main/java/de/eldecker/spring/reminder/model/ReminderException.java
+++ src/main/java/de/eldecker/spring/reminder/model/ReminderException.java
@@ -4,6 +4,7 @@ package de.eldecker.spring.reminder.model;
 /**
  * Applikations-spezifische Exception-Klasse. 
  */
+@SuppressWarnings("serial")
 public class ReminderException extends Exception {
     
     public ReminderException( String fehlerbeschreibung ) {
